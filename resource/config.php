255c62653468ec8fc19a6ad1fcf53cd19e00f9ae
OSAKA-IFTEKHAR <eather@export-japan.co.jp>
2019-05-14 10:21:58 +0900

    updated url and application path

--- resource/config.php
+++ resource/config.php
@@ -1,10 +1,4 @@
 <?php
 
-    /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-define("BASE_URL",$_SERVER['SERVER_ADDR']);
-define("BASE_DIR",'../');
+define("BASE_URL",$_SERVER['HTTP_HOST']);
+define("BASE_DIR",'./');
